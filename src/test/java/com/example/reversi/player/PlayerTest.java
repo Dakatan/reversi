be5af72ced5b9e38d3ce7bf459e1644ed5f3b014
Takada <tkdtkd0022gmail.com>
2018-06-23 21:12:03 +0900

    add strategy

--- src/test/java/com/example/reversi/player/PlayerTest.java
+++ src/test/java/com/example/reversi/player/PlayerTest.java
@@ -1,8 +1,9 @@
 package com.example.reversi.player;
 
+import com.example.reversi.BitBoard;
 import com.example.reversi.Board;
-import com.example.reversi.BoardImpl;
 import com.example.reversi.Stone;
+import com.example.reversi.player.evaluation.DynamicReversiStrategy;
 import org.junit.Test;
 
 public class PlayerTest {
@@ -14,35 +15,42 @@ public class PlayerTest {
   private int draw;
 
   public void setUp() {
-    board = new BoardImpl();
-    player1 = new RandomPlayer();
-    player2 = new AlphaBetaPlayer(5);
+    board = new BitBoard();
+//    board = new BoardImpl();
+    player1 = new AlphaBetaPlayer(5);
+//    player1 = new RandomPlayer();
+    player2 = new AlphaBetaPlayer(5, new DynamicReversiStrategy());
+//    player2 = new RandomPlayer();
+//    player2 = new RandomPlayer();
+//    player2 = new AlphaBetaPlayer(0);
   }
 
   @Test
   public void test() {
-    for (int i = 0; i < 1; i++) {
+    long now = System.currentTimeMillis();
+    for (int i = 0; i < 100; i++) {
       setUp();
       test01();
     }
+    System.out.println(System.currentTimeMillis() - now);
     System.out.println("BLACK: " + black + ", WHITE: " + white + ", DRAW:" + draw);
   }
 
   public void test01() {
-    board.print();
+//    board.print();
     for(int i = 0; i < 100; i++) {
       if(board.isFinish()) break;
       if(board.canPut(Stone.BLACK)) {
         Player.Position pos = player1.play(board, Stone.BLACK);
         board.put(pos.x(), pos.y(), Stone.BLACK);
-        board.print();
+//        board.print();
       }
       if(board.isFinish()) break;
 
       if(board.canPut(Stone.WHITE)) {
         Player.Position pos = player2.play(board, Stone.WHITE);
         board.put(pos.x(), pos.y(), Stone.WHITE);
-        board.print();
+//        board.print();
       }
     }
     if(board.count(Stone.WHITE) > board.count(Stone.BLACK)) {
